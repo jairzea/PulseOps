6594633aa10e642fa3242f7dba0e00de103dc576
Jair Zea <jairzeapaez@gmail.com>
2026-06-25 12:00:25 -0500

    refactor: remove dead files and debug logging

--- apps/frontend/src/components/index.ts
+++ apps/frontend/src/components/index.ts
@@ -5,7 +5,6 @@
 // Toast System
 export { Toast } from './Toast';
 export { ToastContainer } from './ToastContainer';
-export { ToastDemo } from './ToastDemo';
 
 // Otros componentes existentes
 export { MetricModal } from './MetricModal';
